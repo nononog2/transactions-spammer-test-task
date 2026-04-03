d755b20023f16815bf976a10db95080db21c4c3a
nononog2 <43493169+nononog2@users.noreply.github.com>
2026-04-03 13:37:11 +0700

    Add ready-to-use Prometheus and Grafana monitoring

--- platform-core/build.gradle.kts
+++ platform-core/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
